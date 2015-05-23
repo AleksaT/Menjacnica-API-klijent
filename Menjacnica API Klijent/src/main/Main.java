b67c20c92f19462360f8cec6a28e72a12217c939
AleksaT <aleksa.topalovic015@gmail.com>
2015-05-23 23:28:25 +0200

    Kreirana main metoda u klasi Main

---
+++ Menjacnica API Klijent/src/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+
+import json.AzuriranjeKursneListe;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		AzuriranjeKursneListe azk = new AzuriranjeKursneListe();
+		azk.azurirajValute();
+
+	}
+
+}
